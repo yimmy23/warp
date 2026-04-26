61b3a3cf4eaf002a81ce5ca6702aaacd6329e3a5
Zach Bai <zachrybai@gmail.com>
2026-04-26 16:38:15 -0700

    Add feature flag, API binding scaffolding for cloud->cloud handoff.

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -2869,6 +2869,8 @@ pub fn enabled_features() -> HashSet<FeatureFlag> {
         FeatureFlag::CloudModeInputV2,
         #[cfg(feature = "configurable_context_window")]
         FeatureFlag::ConfigurableContextWindow,
+        #[cfg(feature = "handoff_cloud_cloud")]
+        FeatureFlag::HandoffCloudCloud,
     ]);
 
     flags
